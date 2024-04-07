3e502e1fbb9a90216f5181011b8b3d91e5b16bc7
Romeu Lucas <romeulucas50@hotmail.com>
2024-04-06 21:31:30 -0300

    mudanças

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const form = document.getElementById('form');
+const form = document.getElementById('pedido');
 const senhaInput = document.getElementById('senha');
 const senhaButton = document.getElementById('btnSenha');
 const formularioDiv = document.getElementById('pedido');
 
-senhaButton.addEventListener('click', function() {
+senhaButton.addEventListener('click', function () {
     const senhaDigitada = senhaInput.value;
     const senhaCorreta = "como"; // Senha
-    
+
     if (senhaDigitada === senhaCorreta) {
         formularioDiv.style.display = 'block';
-        senhaInput.value = ''; 
+        senhaInput.value = '';
         document.getElementById('senhaForm').style.display = 'none';
     } else {
         alert('Senha incorreta! Tente novamente.');
     }
 });
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    const nome = document.getElementById('nome').value;
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    document.getElementById('totalpedido').style.display = '';
+    document.getElementById('registros').style.display = '';
+
     const pizza = document.getElementById('pizza').value;
-    const quantidade = parseInt(document.getElementById('quantidade').value);
-    const quantidadeTurno = parseInt(document.getElementById('quantidadeTurno').value);
-    const unidade = document.getElementById('unidade').value;
+    const bebida = document.getElementById('bebida').value;
+    const nome = document.getElementById('nome').value;
+    const ende = document.getElementById('ende').value;
+    const entrega = document.getElementById('entrega').value;
 
     const registro = {
         id: Date.now(),
-        nome,
         pizza,
-        quantidade,
-        quantidadeTurno,
-        horario,
-        diasSemana,
-        dataEntrada,
-        dataSaida,
-        unidade
+        bebida,
+        nome,
+        ende,
+        entrega,
     };
 
     adicionarRegistro(registro);
@@ -46,33 +46,30 @@ form.addEventListener('submit', function(event) {
 function adicionarRegistro(registro) {
     const registroBody = document.getElementById('registro-body');
     const newRow = document.createElement('tr');
-    newRow.setAttribute('id', registro.id); 
+    newRow.setAttribute('id', registro.id);
     newRow.innerHTML = `
+        <td>${registro.pizza}</td>
+        <td>${registro.bebida}</td>
         <td>${registro.nome}</td>
-        <td>${registro.curso}</td>
-        <td>${registro.unidade}</td>
-        <td>${registro.quantidade}</td>
-        <td>${registro.quantidadeTurno}</td>
-        <td>${registro.horario.join(', ')}</td>
-        <td>${registro.diasSemana.join(', ')}</td>
-        <td>${registro.dataEntrada}</td>
-        <td>${registro.dataSaida}</td>
+        <td>${registro.ende}</td>
+        <td>${registro.entrega}</td>
     `;
     registroBody.appendChild(newRow);
+    console.log('Formulário enviado');
 }
 
 function salvarRegistro(registro) {
     let registros = JSON.parse(localStorage.getItem('registros')) || [];
-    registros.push(registro); 
-    localStorage.setItem('registros', JSON.stringify(registros)); 
+    registros.push(registro);
+    localStorage.setItem('registros', JSON.stringify(registros));
 }
 
 function removerRegistrosExpirados() {
     let registros = JSON.parse(localStorage.getItem('registros')) || [];
     const agora = new Date().toISOString().split('T')[0];
-    
+
     registros = registros.filter(registro => registro.dataSaida >= agora);
-    
+
     localStorage.setItem('registros', JSON.stringify(registros));
 }
 
